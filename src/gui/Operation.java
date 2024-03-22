b2b0774f46dcbe8e124922a1cf0089adddc53888
tineojf <92743762+tineodev@users.noreply.github.com>
2024-03-22 11:05:23 -0500

    Add gui in operation class

--- src/gui/Operation.java
+++ src/gui/Operation.java
@@ -16,20 +16,199 @@ public class Operation extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        panelTitle = new javax.swing.JPanel();
+        lbTitle = new javax.swing.JLabel();
+        btnLogout = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
+        btnPlace = new javax.swing.JButton();
+        btnCompany = new javax.swing.JButton();
+        btnUser = new javax.swing.JButton();
+        btnTicket = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txtPlace = new javax.swing.JTextArea();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        txtTicket = new javax.swing.JTextArea();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        txtUser = new javax.swing.JTextArea();
+        jScrollPane4 = new javax.swing.JScrollPane();
+        txtCompany = new javax.swing.JTextArea();
+        lbName = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(255, 215, 0));
 
+        panelTitle.setBackground(new java.awt.Color(247, 247, 247));
+
+        lbTitle.setFont(new java.awt.Font("URW Gothic", 1, 48)); // NOI18N
+        lbTitle.setForeground(new java.awt.Color(51, 51, 51));
+        lbTitle.setText("NPB - Operaciones");
+        lbTitle.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+
+        btnLogout.setBackground(new java.awt.Color(255, 215, 0));
+        btnLogout.setFont(new java.awt.Font("URW Gothic", 0, 24)); // NOI18N
+        btnLogout.setForeground(new java.awt.Color(51, 51, 51));
+        btnLogout.setText("Logout");
+        btnLogout.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+
+        javax.swing.GroupLayout panelTitleLayout = new javax.swing.GroupLayout(panelTitle);
+        panelTitle.setLayout(panelTitleLayout);
+        panelTitleLayout.setHorizontalGroup(
+            panelTitleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelTitleLayout.createSequentialGroup()
+                .addGap(61, 61, 61)
+                .addComponent(lbTitle)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnLogout, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(92, 92, 92))
+        );
+        panelTitleLayout.setVerticalGroup(
+            panelTitleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelTitleLayout.createSequentialGroup()
+                .addContainerGap(39, Short.MAX_VALUE)
+                .addGroup(panelTitleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnLogout, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lbTitle))
+                .addGap(22, 22, 22))
+        );
+
+        jPanel2.setBackground(new java.awt.Color(242, 245, 247));
+
+        btnPlace.setBackground(new java.awt.Color(111, 29, 27));
+        btnPlace.setFont(new java.awt.Font("URW Gothic", 0, 36)); // NOI18N
+        btnPlace.setForeground(new java.awt.Color(255, 255, 255));
+        btnPlace.setText("Lugares");
+        btnPlace.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+
+        btnCompany.setBackground(new java.awt.Color(111, 29, 27));
+        btnCompany.setFont(new java.awt.Font("URW Gothic", 0, 36)); // NOI18N
+        btnCompany.setForeground(new java.awt.Color(255, 255, 255));
+        btnCompany.setText("Compañías");
+        btnCompany.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+
+        btnUser.setBackground(new java.awt.Color(111, 29, 27));
+        btnUser.setFont(new java.awt.Font("URW Gothic", 0, 36)); // NOI18N
+        btnUser.setForeground(new java.awt.Color(255, 255, 255));
+        btnUser.setText("Usuarios");
+        btnUser.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+
+        btnTicket.setBackground(new java.awt.Color(111, 29, 27));
+        btnTicket.setFont(new java.awt.Font("URW Gothic", 0, 36)); // NOI18N
+        btnTicket.setForeground(new java.awt.Color(255, 255, 255));
+        btnTicket.setText("Billetes");
+        btnTicket.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+
+        txtPlace.setEditable(false);
+        txtPlace.setBackground(new java.awt.Color(255, 255, 255));
+        txtPlace.setColumns(20);
+        txtPlace.setFont(new java.awt.Font("URW Gothic", 1, 18)); // NOI18N
+        txtPlace.setForeground(new java.awt.Color(30, 30, 30));
+        txtPlace.setLineWrap(true);
+        txtPlace.setRows(3);
+        txtPlace.setText("Son los sitios que serán origen o destino para el billete.  Cada lugar tiene un número y un nombre que lo identifica dentro del sistema.");
+        jScrollPane1.setViewportView(txtPlace);
+
+        txtTicket.setEditable(false);
+        txtTicket.setBackground(new java.awt.Color(255, 255, 255));
+        txtTicket.setColumns(20);
+        txtTicket.setFont(new java.awt.Font("URW Gothic", 1, 18)); // NOI18N
+        txtTicket.setForeground(new java.awt.Color(30, 30, 30));
+        txtTicket.setLineWrap(true);
+        txtTicket.setRows(3);
+        txtTicket.setText("Son los tickets del viaje. Contienen detalles como número de boleto, fecha, hora, lugar de destino y origen, DNI del cliente  y el ID de la compañía.");
+        jScrollPane2.setViewportView(txtTicket);
+
+        txtUser.setEditable(false);
+        txtUser.setBackground(new java.awt.Color(255, 255, 255));
+        txtUser.setColumns(20);
+        txtUser.setFont(new java.awt.Font("URW Gothic", 1, 18)); // NOI18N
+        txtUser.setForeground(new java.awt.Color(30, 30, 30));
+        txtUser.setLineWrap(true);
+        txtUser.setRows(3);
+        txtUser.setText("Representan las personas que utilizan la aplicación. Tienen información como el DNI, contraseña, nombre, teléfono y dirección.");
+        jScrollPane3.setViewportView(txtUser);
+
+        txtCompany.setEditable(false);
+        txtCompany.setBackground(new java.awt.Color(255, 255, 255));
+        txtCompany.setColumns(20);
+        txtCompany.setFont(new java.awt.Font("URW Gothic", 1, 18)); // NOI18N
+        txtCompany.setForeground(new java.awt.Color(30, 30, 30));
+        txtCompany.setLineWrap(true);
+        txtCompany.setRows(3);
+        txtCompany.setText("Son las entidades comerciales relacionadas con la prestación de servicios. Tienen un número único y un nombre que las identifica.");
+        jScrollPane4.setViewportView(txtCompany);
+
+        lbName.setFont(new java.awt.Font("URW Gothic", 1, 18)); // NOI18N
+        lbName.setForeground(new java.awt.Color(30, 30, 30));
+        lbName.setText("BIENVENIDO, ");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addGroup(jPanel2Layout.createSequentialGroup()
+                            .addContainerGap()
+                            .addComponent(btnPlace, javax.swing.GroupLayout.PREFERRED_SIZE, 239, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel2Layout.createSequentialGroup()
+                            .addGap(93, 93, 93)
+                            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addComponent(btnUser, javax.swing.GroupLayout.PREFERRED_SIZE, 239, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(btnCompany))))
+                    .addComponent(btnTicket, javax.swing.GroupLayout.PREFERRED_SIZE, 239, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 63, Short.MAX_VALUE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 521, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 521, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 521, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane4, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 521, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(34, 34, 34))
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(93, 93, 93)
+                .addComponent(lbName, javax.swing.GroupLayout.PREFERRED_SIZE, 358, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(30, 30, 30)
+                .addComponent(lbName)
+                .addGap(47, 47, 47)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnTicket, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(31, 31, 31)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnPlace, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(46, 46, 46)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                        .addComponent(btnCompany, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(10, 10, 10)))
+                .addGap(39, 39, 39)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnUser, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(72, Short.MAX_VALUE))
+        );
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 950, Short.MAX_VALUE)
+            .addComponent(panelTitle, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 730, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(panelTitle, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -48,6 +227,23 @@ public class Operation extends javax.swing.JFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCompany;
+    private javax.swing.JButton btnLogout;
+    private javax.swing.JButton btnPlace;
+    private javax.swing.JButton btnTicket;
+    private javax.swing.JButton btnUser;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JScrollPane jScrollPane4;
+    private javax.swing.JLabel lbName;
+    private javax.swing.JLabel lbTitle;
+    private javax.swing.JPanel panelTitle;
+    private javax.swing.JTextArea txtCompany;
+    private javax.swing.JTextArea txtPlace;
+    private javax.swing.JTextArea txtTicket;
+    private javax.swing.JTextArea txtUser;
     // End of variables declaration//GEN-END:variables
 }
