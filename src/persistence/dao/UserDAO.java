8060358eb09eb19481c01d86b3752b1620b7a903
tineojf <92743762+tineodev@users.noreply.github.com>
2024-03-23 21:38:46 -0500

    Add getuserid to userdao

--- src/persistence/dao/UserDAO.java
+++ src/persistence/dao/UserDAO.java
@@ -36,4 +36,28 @@ public class UserDAO {
             return null;
         }
     }
+
+    public static User getUserID(int id) {
+        String query = "SELECT * FROM CLIENTES WHERE DNI = " + String.valueOf(id);
+
+        try {
+            ResultSet result = connection.createStatement().executeQuery(query);
+
+            if (result.next()) {
+                User usuario = new User(
+                        String.valueOf(result.getInt("DNI")),
+                        result.getString("NOMBRE"),
+                        result.getString("PASSWORD"),
+                        result.getString("TELEFONO"),
+                        result.getString("DIRECCION")
+                );
+                return usuario;
+            }
+
+            return null;
+        } catch (SQLException e) {
+            System.out.println("Select error: " + e.getMessage());
+            return null;
+        }
+    }
 }
