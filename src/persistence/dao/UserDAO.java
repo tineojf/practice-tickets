f32d33afe151a882ef0e8d4274b3a5a503ac7d63
tineojf <92743762+tineodev@users.noreply.github.com>
2024-03-23 21:32:05 -0500

    Add userdao

---
+++ src/persistence/dao/UserDAO.java
@@ -0,0 +1,39 @@
+package persistence.dao;
+
+import persistence.ConnectorDB;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import persistence.models.User;
+
+public class UserDAO {
+
+    private static final ConnectorDB connector = ConnectorDB.getInstance();
+    private static final Connection connection = connector.getConnection();
+
+    // Get - users
+    public static ArrayList<User> getUsers() {
+        ArrayList<User> userList = new ArrayList<>();
+        String query = "SELECT * FROM CLIENTES";
+
+        try {
+            ResultSet result = connection.createStatement().executeQuery(query);
+
+            while (result.next()) {
+                userList.add(new User(
+                        String.valueOf(result.getInt("DNI")),
+                        result.getString("NOMBRE"),
+                        result.getString("PASSWORD"),
+                        result.getString("TELEFONO"),
+                        result.getString("DIRECCION")
+                ));
+            }
+
+            return userList;
+        } catch (SQLException e) {
+            System.out.println("Select error: " + e.getMessage());
+            return null;
+        }
+    }
+}
